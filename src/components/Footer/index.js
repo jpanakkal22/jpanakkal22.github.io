8f09e1b8fabe4e02707599151c3f91a76e44e50d
New Dad <josh_panakkal3122@hotmail.com>
2020-10-15 16:09:17 -0400

    initial commit of files / add footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function Footer() {
+  return (
+    <footer className="footer container-fluid">
+      <div className="row">
+        <div className="col-md-12">
+        <h6><i class="fa fa-coffee" aria-hidden="true"></i> Enjoy!</h6>
+        </div>
+      </div>      
+    </footer>
+  );
+}
+
+export default Footer;
