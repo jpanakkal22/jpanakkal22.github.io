7c34052d7363d7082f3ac524d63383f4b6020a35
New Dad <josh_panakkal3122@hotmail.com>
2020-10-15 16:28:31 -0400

    add header and style

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./style.css";
+
+function Header(){
+    return(        
+    <div className="page-header">
+        <h1>Josh Panakkal</h1>      
+    </div>      
+    );
+}
+
+export default Header;
