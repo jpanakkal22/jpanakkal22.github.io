ef127ccd4636f6785811ac39c2f99e35265d0e6e
New Dad <josh_panakkal3122@hotmail.com>
2020-10-19 18:24:39 -0400

    Updates

---
+++ precache-manifest.263870c94feb2d5ea4f71c3501c48be1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae38b98aa2dda1a1c74773dbd03ad487",
+    "url": "/index.html"
+  },
+  {
+    "revision": "521354098ffd5c217613",
+    "url": "/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "6543dea428b0fdae08f9",
+    "url": "/static/css/main.fde80052.chunk.css"
+  },
+  {
+    "revision": "521354098ffd5c217613",
+    "url": "/static/js/2.8dd78951.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.8dd78951.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6543dea428b0fdae08f9",
+    "url": "/static/js/main.9a725a8d.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "d6e7548ae6ff28c478cfae5d0807fdab",
+    "url": "/static/media/profile-picture.d6e7548a.jpg"
+  }
+]);
