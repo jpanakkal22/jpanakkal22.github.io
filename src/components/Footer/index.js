a9065133881d4bf0ab8b229f7c64ab3bf6c5e658
New Dad <josh_panakkal3122@hotmail.com>
2020-12-19 22:27:01 -0500

    update footer content and styling

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { faCopyright } from '@fortawesome/free-regular-svg-icons';
 import "./style.css";
 
 function Footer() {
@@ -6,7 +9,8 @@ function Footer() {
     <footer className="footer container-fluid">
       <div className="row">
         <div className="col-md-12">
-        <h6><i className="fa fa-coffee" aria-hidden="true"></i> Enjoy!</h6>
+        <h6><FontAwesomeIcon icon={faCoffee} /> Enjoy! <FontAwesomeIcon icon={faCopyright} /> J.Panakkal 2020</h6>
+        
         </div>
       </div>      
     </footer>
