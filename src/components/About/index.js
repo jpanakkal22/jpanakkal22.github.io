26b88f0336dd8bccb27a02aa57dfa90d47ac2b12
New Dad <josh_panakkal3122@hotmail.com>
2020-12-19 22:28:57 -0500

    add About folder and files, content and styling

---
+++ src/components/About/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+
+function About() {
+    return (
+        <div id="aboutDiv">
+            <h4>ABOUT</h4>
+            <p className="footerPara">Full Stack Web Developer (MERN Stack) <br />
+            Case Western Reserve Coding Bootcamp Graduate <br />
+            13 Years Industrial Maintenance Background <br />
+            Father, Husband, Cyclist / MTB</p>
+        </div>
+    )
+}
+
+export default About;
